88c0ac64916000568e0229f2cac0278da85f56e1
Soni kumari <Sonikumari78707@gmail.com>
2023-12-25 01:21:31 +0530

    coding

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const quizData = [{
-	question: "Which of the following is a client site language?",
-	a: "Java",
-	b: "C",
-	c: "Python",
-	d: "JavaScript",
-	correct: "d",
+const questions = [
+{
+	'que': "Which of the following is a client site language?",
+	'a': "Java",
+	'b': "C",
+	'c': "Python",
+	'd': "JavaScript",
+	'correct': "d",
 },
 {
-	question: "What does HTML stand for?",
+	que: "What does HTML stand for?",
 	a: "Hypertext Markup Language",
 	b: "Cascading Style Sheet",
 	c: "Jason Object Notation",
@@ -15,7 +16,7 @@ const quizData = [{
 	correct: "a",
 },
 {
-	question: "What year was JavaScript launched?",
+	que: "What year was JavaScript launched?",
 	a: "1996",
 	b: "1995",
 	c: "1994",
@@ -23,11 +24,30 @@ const quizData = [{
 	correct: "b",
 },
 {
-	question: "What does CSS stands for?",
-	a: "Hypertext Markup Language",
-	b: "Cascading Style Sheet",
-	c: "Jason Object Notation",
-	d: "Helicopters Terminals Motorboats Lamborginis",
-	correct: "b",
+	'que': "What does CSS stands for?",
+	'a': "Hypertext Markup Language",
+	'b': "Cascading Style Sheet",
+	'c': "Jason Object Notation",
+	'd': "Helicopters Terminals Motorboats Lamborginis",
+	'correct': "b",
 }
 ];
+
+let index = 3;
+const quesBox =document.getElementById("quesBox");
+const optInp =document.querySelectorAll('.opts');
+
+
+const loadQuestion =()=>{
+	const data = questions[index]
+	console.log(data)
+	quesBox.innerText=` ${index+1}) ${data.que}`;
+	
+    optInp[0].nextElementSibling.innerText = data.a
+    optInp[1].nextElementSibling.innerText = data.b
+    optInp[2].nextElementSibling.innerText = data.c
+    optInp[3].nextElementSibling.innerText = data.d
+}
+
+// initial call reload page
+loadQuestion();
